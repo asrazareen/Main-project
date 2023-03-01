71f6e99111f8ccc0d8c7e1a1291613afe7c21557
rizviabbas128 <108932526+rizviabbas128@users.noreply.github.com>
2023-03-01 21:00:12 +0530

    Backend for Login Register Logout

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./schema');
+const jwt = require('jsonwebtoken');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+app.use(cors());
+app.use(express.json());
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+mongoose.set('strictQuery', false)
+mongoose.connect(`mongodb+srv://mainproject:mainproject@cluster0.isbhiqp.mongodb.net/?retryWrites=true&w=majority`,(err) => {
+    if(err) {
+        console.log("Connection to MongoDb Failed");
+    }else {
+        console.log("Succesfully connected to MongoDb");
+    }
+});
+
+app.post('/api/register', async (req, res) => {
+    console.log(req.body);
+    try {
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            profession: req.body.profession,
+            password: req.body.password,
+            confirmpassword: req.body.confirmpassword,
+        }) 
+        if(req.body.password === req.body.confirmpassword) {
+            res.json({ status: 'ok' })
+        }else {
+            res.json({status: 'password and confirmpassword should be same'})
+        }
+    } catch (err) {
+        res.json({ status: 'error', error: 'Duplicate email' })
+    }
+})
+
+app.post('/api/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email, password: req.body.password })
+
+    if (user) {
+        const token = jwt.sign({
+            name: user.name,
+            email: user.email,
+        }, 'secret123')
+        return res.json({ status: 'ok', token:token})
+    } else {
+        return res.json({ status: 'error', user: false })
+    }
+    }catch(error) {
+        res.json(error);
+    }
+})
+
+
+app.listen(8000, () => console.log("Server is up at port 8000"));
